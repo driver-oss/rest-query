702b86748425650445b8b2278814474bab9ae27b
vlad <vlad@driver.xyz>
2017-07-14 11:15:25 -0700

    Make AuthenticatedRequestContext to not request executor

--- src/main/scala/xyz/driver/pdsuicommon/auth/AuthenticatedRequestContext.scala
+++ src/main/scala/xyz/driver/pdsuicommon/auth/AuthenticatedRequestContext.scala
@@ -4,8 +4,10 @@ import xyz.driver.entities.users.UserInfo
 import xyz.driver.pdsuicommon.logging._
 import xyz.driver.pdsuicommon.domain.User
 
-class AuthenticatedRequestContext(val executor: User, val driverUser: UserInfo, override val requestId: RequestId)
+class AuthenticatedRequestContext(val driverUser: UserInfo, override val requestId: RequestId)
     extends AnonymousRequestContext(requestId) {
+
+  val executor: User = new User(driverUser)
 
   override def equals(that: Any): Boolean = {
     that.getClass == this.getClass && {
@@ -23,8 +25,8 @@ class AuthenticatedRequestContext(val executor: User, val driverUser: UserInfo, 
 
 object AuthenticatedRequestContext {
 
-  def apply(executor: User, driverUser: UserInfo) =
-    new AuthenticatedRequestContext(executor, driverUser, RequestId())
+  def apply(driverUser: UserInfo) =
+    new AuthenticatedRequestContext(driverUser, RequestId())
 
   implicit def toPhiString(x: AuthenticatedRequestContext): PhiString = {
     phi"AuthenticatedRequestContext(executor=${x.executor}, requestId=${x.requestId})"
