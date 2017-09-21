8166ef497437567c8e7aaa9b2a8d6482752c017b
Aleksandr <ognelisar@gmail.com>
2017-09-21 16:42:49 +0700

    Fixed meta attribute filling of MedicalRecord when one transforms it from ApiRecord; Fixed FuzzyValue fromString method: on proviedstring it transformes the string to lower case and capitalize it

--- src/main/scala/xyz/driver/pdsuidomain/formats/json/record/ApiRecord.scala
+++ src/main/scala/xyz/driver/pdsuidomain/formats/json/record/ApiRecord.scala
@@ -13,6 +13,54 @@ import play.api.libs.json._
 import xyz.driver.pdsuicommon.json.JsonSerializer
 import xyz.driver.pdsuicommon.domain.{LongId, StringId, TextJson, UuidId}
 
+object ApiRecord {
+
+  private val emptyMeta: String =
+    "[]"
+
+  private val statusFormat = Format(
+    Reads.StringReads.filter(ValidationError("unknown status")) {
+      case x if MedicalRecordStatus.statusFromString.isDefinedAt(x) => true
+      case _                                                        => false
+    },
+    Writes.StringWrites
+  )
+
+  implicit val format: Format[ApiRecord] = (
+    (JsPath \ "id").format[Long] and
+      (JsPath \ "patientId").format[String] and
+      (JsPath \ "caseId").formatNullable[String] and
+      (JsPath \ "disease").format[String] and
+      (JsPath \ "physician").formatNullable[String] and
+      (JsPath \ "lastUpdate").format[ZonedDateTime] and
+      (JsPath \ "status").format(statusFormat) and
+      (JsPath \ "previousStatus").formatNullable(statusFormat) and
+      (JsPath \ "assignee").formatNullable[String] and
+      (JsPath \ "previousAssignee").formatNullable[String] and
+      (JsPath \ "lastActiveUser").formatNullable[String] and
+      (JsPath \ "requestId").format[UUID] and
+      (JsPath \ "meta").format(Format(Reads { x =>
+        JsSuccess(Json.stringify(x))
+      }, Writes[String](Json.parse)))
+    )(ApiRecord.apply, unlift(ApiRecord.unapply))
+
+  def fromDomain(record: MedicalRecord) = ApiRecord(
+    id = record.id.id,
+    patientId = record.patientId.toString,
+    caseId = record.caseId.map(_.id),
+    disease = record.disease,
+    physician = record.physician,
+    lastUpdate = ZonedDateTime.of(record.lastUpdate, ZoneId.of("Z")),
+    status = MedicalRecordStatus.statusToString(record.status),
+    previousStatus = record.previousStatus.map(MedicalRecordStatus.statusToString),
+    assignee = record.assignee.map(_.id),
+    previousAssignee = record.previousAssignee.map(_.id),
+    lastActiveUser = record.lastActiveUserId.map(_.id),
+    requestId = record.requestId.id,
+    meta = record.meta.map(x => JsonSerializer.serialize(x.content)).getOrElse(emptyMeta)
+  )
+}
+
 final case class ApiRecord(id: Long,
                            patientId: String,
                            caseId: Option[String],
@@ -46,55 +94,15 @@ final case class ApiRecord(id: Long,
     disease = this.disease,
     caseId = caseId.map(CaseId(_)),
     physician = this.physician,
-    meta = Some(TextJson(JsonSerializer.deserialize[List[MedicalRecord.Meta]](this.meta))),
+    meta = {
+      if (this.meta == ApiRecord.emptyMeta) {
+        None
+      } else {
+        Some(TextJson(JsonSerializer.deserialize[List[MedicalRecord.Meta]](this.meta)))
+      }
+    },
     predictedMeta = None,
     predictedDocuments = None,
     lastUpdate = this.lastUpdate.toLocalDateTime()
-  )
-
-}
-
-object ApiRecord {
-
-  private val statusFormat = Format(
-    Reads.StringReads.filter(ValidationError("unknown status")) {
-      case x if MedicalRecordStatus.statusFromString.isDefinedAt(x) => true
-      case _                                                        => false
-    },
-    Writes.StringWrites
-  )
-
-  implicit val format: Format[ApiRecord] = (
-    (JsPath \ "id").format[Long] and
-      (JsPath \ "patientId").format[String] and
-      (JsPath \ "caseId").formatNullable[String] and
-      (JsPath \ "disease").format[String] and
-      (JsPath \ "physician").formatNullable[String] and
-      (JsPath \ "lastUpdate").format[ZonedDateTime] and
-      (JsPath \ "status").format(statusFormat) and
-      (JsPath \ "previousStatus").formatNullable(statusFormat) and
-      (JsPath \ "assignee").formatNullable[String] and
-      (JsPath \ "previousAssignee").formatNullable[String] and
-      (JsPath \ "lastActiveUser").formatNullable[String] and
-      (JsPath \ "requestId").format[UUID] and
-      (JsPath \ "meta").format(Format(Reads { x =>
-        JsSuccess(Json.stringify(x))
-      }, Writes[String](Json.parse)))
-  )(ApiRecord.apply, unlift(ApiRecord.unapply))
-
-  def fromDomain(record: MedicalRecord) = ApiRecord(
-    id = record.id.id,
-    patientId = record.patientId.toString,
-    caseId = record.caseId.map(_.id),
-    disease = record.disease,
-    physician = record.physician,
-    lastUpdate = ZonedDateTime.of(record.lastUpdate, ZoneId.of("Z")),
-    status = MedicalRecordStatus.statusToString(record.status),
-    previousStatus = record.previousStatus.map(MedicalRecordStatus.statusToString),
-    assignee = record.assignee.map(_.id),
-    previousAssignee = record.previousAssignee.map(_.id),
-    lastActiveUser = record.lastActiveUserId.map(_.id),
-    requestId = record.requestId.id,
-    meta = record.meta.map(x => JsonSerializer.serialize(x.content)).getOrElse("[]")
   )
 }
