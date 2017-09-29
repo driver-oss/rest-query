5750f2f3633e75f2f96d6a36264ab4b8f3fec7d2
timgushue <tim@driver.xyz>
2017-09-29 12:18:16 -0700

    Added SlotArmEligibilityArm entity

---
+++ src/main/scala/xyz/driver/pdsuidomain/entities/SlotArmEligibilityArm.scala
@@ -0,0 +1,15 @@
+package xyz.driver.pdsuidomain.entities
+
+import xyz.driver.pdsuicommon.domain.LongId
+import xyz.driver.pdsuicommon.logging._
+
+final case class SlotArmEligibilityArm(slotArmId: LongId[SlotArm],
+                                       eligibilityArmId: LongId[EligibilityArm])
+
+
+object SlotArmEligibilityArm {
+  implicit def toPhiString(x: SlotArmEligibilityArm): PhiString = {
+    import x._
+    phi"SlotArmEligibilityArm(slotArmId=$slotArmId, eligibilityArmId=$eligibilityArmId)"
+  }
+}
