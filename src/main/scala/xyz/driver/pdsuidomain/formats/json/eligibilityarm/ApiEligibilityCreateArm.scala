7696399c9c8e1519b19f6bf7b6eec515eb03b0d7
Marvin Bertin <marvin.bertin@gmail.com>
2017-10-02 15:08:11 -0700

    update ApiCreateEligibilityArm

--- src/main/scala/xyz/driver/pdsuidomain/formats/json/eligibilityarm/ApiEligibilityCreateArm.scala
+++ src/main/scala/xyz/driver/pdsuidomain/formats/json/eligibilityarm/ApiEligibilityCreateArm.scala
@@ -1,17 +1,25 @@
 package xyz.driver.pdsuidomain.formats.json.eligibilityarm
 
 import xyz.driver.pdsuicommon.domain.{LongId, StringId}
-import xyz.driver.pdsuidomain.entities.EligibilityArm
+import xyz.driver.pdsuidomain.entities.{EligibilityArm, EligibilityArmDisease, EligibilityArmWithDiseases, Trial}
 import play.api.libs.json.{Format, Json}
 
-final case class ApiCreateEligibilityArm(name: String, trialId: String) {
+final case class ApiCreateEligibilityArm(name: String, trialId: String, diseases: Seq[String]) {
 
-  def toDomain = EligibilityArm(
-    id = LongId(0),
-    name = name,
-    trialId = StringId(trialId),
-    originalName = name
-  )
+  def toDomain: EligibilityArmWithDiseases = {
+    val eligibilityArm = EligibilityArm(
+      id = LongId(0),
+      name = name,
+      trialId = StringId(trialId),
+      originalName = name
+    )
+
+    EligibilityArmWithDiseases(eligibilityArm, diseases.map { disease =>
+      val condition = Trial.Condition.fromString(disease)
+        .getOrElse(throw new NoSuchElementException(s"unknown condition $disease"))
+      EligibilityArmDisease(eligibilityArm.id, condition)
+    })
+  }
 }
 
 object ApiCreateEligibilityArm {
