f58eeb490fc8f829a5c8f727ee354bfad51c721a
Jakob Odersky <jakob@driver.xyz>
2017-11-20 21:49:42 -0800

    Fix trial ID in update route

--- src/main/scala/xyz/driver/pdsuidomain/services/rest/RestPatientEligibleTrialService.scala
+++ src/main/scala/xyz/driver/pdsuidomain/services/rest/RestPatientEligibleTrialService.scala
@@ -73,7 +73,7 @@ class RestPatientEligibleTrialService(transport: ServiceTransport, baseUri: Uri)
       entity <- Marshal(draftPatientTrialArmGroup).to[RequestEntity]
       request = HttpRequest(
         HttpMethods.PATCH,
-        endpointUri(baseUri, s"/v1/patient/${origEligibleTrialWithTrial.group.patientId}/trial/${origEligibleTrialWithTrial.trial.id}"))
+        endpointUri(baseUri, s"/v1/patient/${origEligibleTrialWithTrial.group.patientId}/trial/${origEligibleTrialWithTrial.group.id}"))
         .withEntity(entity)
       response <- transport.sendRequestGetResponse(requestContext)(request)
       reply    <- apiResponse[RichPatientEligibleTrial](response)
